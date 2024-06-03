0307c32f553afd5f484ca65ea050116f6be13fd7
Juan Sáez García <saez@oscala.es>
2024-06-03 18:45:38 +0200

    eureka

--- products/src/main/java/com/products/models/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.products.models.repository;
-
-import com.products.models.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
